c493472edc04065a0ae4a545aefa41c3b650d93b
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2018-08-05 21:43:23 +0900

    modified code

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
-export function initForm(selector, schema){
+import 'jsonform';
+import 'bootstrap/js/dist/dropdown';
+
+var requiredValues = []
+
+var initForm = function (selector, schema) {
+
+  schema.onSubmit = function (error, values) {
+
+    console.log("value submitted - ")
+    console.log(values)
+
+    if (error) {
+      console.error(error)
+      if (window.ExternalHandler) {
+        window.ExternalHandler.onSubmitted(error, values)
+      }
+      return;
+    }
+
+    var emptyKeys = []
+
+    //check
+    for (const requiredKey of requiredValues) {
+      if (values[requiredKey] == null) {
+        emptyKeys.push(requiredKey)
+      } else if (values[requiredKey] instanceof Array && values[requiredKey].length === 0) {
+        emptyKeys.push(requiredKey)
+      }
+    }
+    if (emptyKeys.length === 0) {
+      if (window.ExternalHandler) {
+        window.ExternalHandler.onSubmitted(null, values)
+      }
+    } else {
+      const err = {
+        error: "RequiredFields",
+        unsetFields: emptyKeys,
+        unsetFieldTitles: emptyKeys.map(key => {
+          return schema.schema[key].title || key
+        })
+      }
+      console.error(err)
+      window.ExternalHandler.onSubmitted(err, values)
+    }
+  }
+
+  requiredValues = []
+  for (const key of Object.keys(schema.schema)) {
+    if (schema.schema[key].required === true) {
+      requiredValues.push(key)
+    }
+  }
+
+  console.log(requiredValues)
+
   $(selector).html("");
   $(selector).jsonForm(schema)
+}
+
+var submit = function () {
+  $('form.jsonform-container').submit(function (event) {
+    event.preventDefault();
+    console.log(event)
+  })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,61 @@
 const path = require('path');
-const SassPlugin = require('sass-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    form: [
-      './node_modules/jquery/dist/jquery.min.js',
-      './node_modules/underscore/underscore-min.js',
-      './node_modules/jsonform/lib/jsonform.js',
-      './index.js'
-    ]
+    index: './index.js',
+    styles: './styles.scss'
+  },
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery',
+      jsonform: './node_modules/jsonform/lib/jsonform.js'
+    }
   },
   module: {
-    rules: [{
-      test: require.resolve('jquery'),
-      loader: 'expose-loader?$!expose-loader?jQuery!expose-loader?window.$!expose-loader?window.jQuery'
-    }, ]
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      }
+    ]
   },
   plugins: [
-    new SassPlugin('./styles.scss', {
-      sass: {
-        outputStyle: 'compressed'
-      },
-      sourceMap: false
-    }),
     new webpack.ProvidePlugin({
       $: 'jquery',
-      jQuery: 'jquery'
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      title: 'Client Questionnaire View',
+      template: './index.html',
+      inject: 'head',
     })
   ],
   output: {
-    filename: 'form.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'built')
   }
 };
